b13f2d07cee7000f63269108b40871457d3b619e
Matheus Sixel <msixel22@gmail.com>
2025-07-28 20:28:11 -0300

    criando página confirm

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
+
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
@@ -12,9 +14,13 @@ const [category, serCategory] = useState("")
 const [isLoading, setIsLoading] = useState(false)
 const [filename, setFilename] = useState<File | null>(null)
 
+const navigate = useNavigate()
+
 function onSubmit(e: React.FormEvent) {
   e.preventDefault()
+
   console.log(name, amount, category, filename)
+  navigate("/confirm", {state: {fromSubmit: true}})
 }
 
   return (
