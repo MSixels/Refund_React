dd9c266e8a607a5155f1b59037b2796ea34e6270
Matheus Sixel <msixel22@gmail.com>
2025-07-28 19:48:17 -0300

    criando component de Upload

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { Upload } from "../components/Upload";
 
 export function Refund() {
 const [category, serCategory] = useState("")
@@ -12,14 +13,22 @@ const [category, serCategory] = useState("")
         <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
       </header>
+
       <Input required legend="Nome da solicitação"/>
-      <Select required legend="Categoria" value={category} onChange={(e) => serCategory(e.target.value)}>
+
+      <div className="flex gap-4">
+        <Select required legend="Categoria" value={category} onChange={(e) => serCategory(e.target.value)}>
         {CATEGORIES_KEYS.map((category) => (
             <option key={category} value={category}>
               {CATEGORIES[category].name}
             </option>
           ))}
       </Select>
+
+      <Input legend="Valor" required/>
+      </div>
+
+      <Upload filename="Matheus.png"/>
     </form>
   )
 }
