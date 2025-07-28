495e1e54ef40328cf12b6c39284be173c191b8ef
Matheus Sixel <msixel22@gmail.com>
2025-07-28 17:05:32 -0300

    criando componente input

--- src/pages/SigIn.tsx
+++ src/pages/SigIn.tsx
@@ -1,3 +1,8 @@
+import { Input } from "../components/Input"
+
 export function SigIn() {
-  return <h1>SigIn</h1>
+  return <form className="w-full flex flex-col gap-4">
+    <Input required legend="E-mail" type="email" placeholder="seu@email.com"/>
+    <Input required legend="Senha" type="password" placeholder="123456"/>
+  </form>
 }
