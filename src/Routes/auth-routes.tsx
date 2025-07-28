bed9f3b39074479d96f067144b51768e04f2e569
Matheus Sixel <msixel22@gmail.com>
2025-07-28 16:48:32 -0300

    criando layout padrão nas telas sign-in e sign-up

--- src/Routes/auth-routes.tsx
+++ src/Routes/auth-routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router";
 
+import { AuthLayout } from "../components/AuthLayout";
 import { SigIn } from "../pages/SigIn";
 
 export function AuthRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<AuthLayout />}>
       <Route path="/" element={<SigIn/>} />
+      </Route>
     </Routes>
   )
 }
