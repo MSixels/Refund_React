b3d56f885a87dbf3e49bc2e7bba82ad1fe03ccee
Matheus Sixel <msixel22@gmail.com>
2025-07-29 13:00:53 -0300

    ajustando navegação no pagination

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,13 +9,13 @@ const variants = {
   button: {
     base: "h-12",
     icon: "h-12 w-12",
-    iconSmall: "h-10 w-10",
+    iconSmall: "h-8 w-8",
   },
 }
 
 export function Button({ children, isLoading, className, type= "button", variant= "base", ...rest }: Props) {
   return <button type={type} disabled={isLoading} {...rest} className={classMerge([
-    "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress", variants.button[variant], className
+    "flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 ", variants.button[variant], isLoading && "cursor-progress", className
   ])
     }>
     {children}
