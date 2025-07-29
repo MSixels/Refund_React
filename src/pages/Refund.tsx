3c53ad57520132b275253caeba2810dac4936bdd
Matheus Sixel <msixel22@gmail.com>
2025-07-29 13:37:31 -0300

    Criando componente Loading e ajustando botão de arquivo

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
+import fileSvg from "../assets/file.svg"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
@@ -48,8 +49,14 @@ function onSubmit(e: React.FormEvent) {
 
       <Input legend="Valor" required value={amount} onChange={(e) => setAmount(e.target.value)} disabled={!!params.id}/>
       </div>
+      {
+        params.id ? (<a href="https://x.com/home?lang=pt" target="_blank" className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear ">
+          <img src={fileSvg} alt="Ícone de arquivo" />Abrir comprovante
+        </a>) : (
+          <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
+        )
+      }
 
-      <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])} />
 
       <Button type="submit" isLoading={isLoading}>{ params.id ? "Voltar" : "Enviar"}</Button>
     </form>
